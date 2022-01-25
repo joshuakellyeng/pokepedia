f1d3ec7125fa202c110b227b7bc8874e6ae25e4a
Joshua Kelly <joshuakellyeng@gmail.com>
2022-01-25 14:19:11 -0500

    added the links to the nav bar using react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useContext } from 'react';
+import { Routes, Route  } from 'react-router-dom';
+
 //components
 import Nav from './components/Nav';
+//context
+import UserContext from './contexts/UserContext';
+//pages
+import Login from './pages/Login';
 
 //css
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <UserContext.Provider value={'Christina'}>
         <Nav />
-       
+        {/* We need to wrap all of our routes insde react router routes component */}
+        <Routes>
+          <Route path='login' element={<Login/>} />
+        </Routes>
+
+      </UserContext.Provider>
     </div>
   );
 }
---
+++ src/contexts/UserContext.js
@@ -0,0 +1,10 @@
+//import createContext
+import { createContext } from "react";
+
+//Simplest way to create a context
+//sp = standard practice
+//SP: name your context the same as your file
+
+const UserContext = createContext(null)
+
+export default UserContext
